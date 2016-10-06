474103512e42474a7dba975873048250ab52edfd
mauriludemann <mauriludemann@gmail.com>
2016-10-06 11:42:05 -0300

    Bugs were fixed

--- src/Petri/PetriNet.java
+++ src/Petri/PetriNet.java
@@ -135,6 +135,7 @@ public abstract class PetriNet {
 		for(int i = 0; i < currentMarking.length; i++){
 			if(resetMatrix[i][transitionIndex] == 1){
 				currentMarking[i] = 0;
+				places[i].setMarking(currentMarking[i]);
 			}
 		}
 		return true;
